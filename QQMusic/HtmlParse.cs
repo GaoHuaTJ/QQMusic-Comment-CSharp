15de46d012f8ecfb1fa046f2520f9a37d0939243
qidaxiang66@icloud.com <qidaxiang66@icloud.com>
2019-02-22 22:11:32 +0800

    不能说的秘密评论C#版

---
+++ QQMusic/HtmlParse.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+using System.Data.SqlClient;
+namespace QQMusic
+{
+    class HtmlParse
+    {
+
+
+        public void GetContent(string path)
+        {
+            var users= GetContents(path, "//h4[@class='comment__title']");
+            var dates = GetContents(path, "//span[@class='comment__date c_tx_thin']");
+            var comments = GetContents(path, "//p[@class='c_tx_normal comment__text js_hot_text']");
+            var agree = GetContents(path, "//span[@class='js_praise_num']");
+
+            for (int i = 0; i < users.Count; i++)
+            {
+                try
+                {
+                    string sql = string.Format("INSERT INTO dbo.不能说的秘密(ID, 用户名,评论,评论时间,赞)VALUES({0},'{1}','{2}','{3}','{4}')", i, users[i], comments[i], dates[i], agree[i]);
+                    ConnectAccess(sql);
+                }
+                catch
+                {break;}
+            }
+            Console.ReadKey();
+    
+            }
+        public List<string> GetContents(string path,string Xpath)
+        {
+                List<string> Contents = new List<string> { };
+                var htmlNodes = GetNodes(path, Xpath);
+                if (htmlNodes != null)
+                {
+                    foreach (var item1 in htmlNodes)//检索评论
+                    {
+                        var Content = item1.InnerText;
+                        Contents.Add(Content);
+                    }
+                }
+            return Contents;
+        }
+        public HtmlNodeCollection GetNodes(string path,string Xpath)
+        {
+            HtmlDocument html = new HtmlDocument();
+            html.Load(path, Encoding.UTF8);//加载html文件
+            HtmlNodeCollection htmlNodes = html.DocumentNode.SelectNodes(Xpath);
+            return htmlNodes;
+        }
+        public void ConnectAccess(string Sql)
+        {
+            string Conn = string.Format("Data Source=DESKTOP-49O35N0;Initial Catalog=C#练习使用;Integrated Security=True");
+            SqlConnection sqlConnection = new SqlConnection(Conn);
+            sqlConnection.Open();
+            SqlCommand sqlCommand = new SqlCommand(Sql, sqlConnection);
+            sqlCommand.ExecuteReader();
+            Console.WriteLine("数据存储成功");
+        }
+    }
+}
--- QQMusic/Program.cs
+++ QQMusic/Program.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using OpenQA.Selenium.Chrome;
+using System.IO;
+using System.Threading;
+using OpenQA.Selenium;
 
 namespace QQMusic
 {
@@ -10,6 +14,34 @@ namespace QQMusic
     {
         static void Main(string[] args)
         {
+            ChromeDriver chromeDriver = new ChromeDriver();
+            var url = "https://y.qq.com/n/yqq/song/002MXZNu1GToOk.html#comment_box";//网址
+            chromeDriver.Navigate().GoToUrl(url);//登陆网址
+            Thread.Sleep(5000);//等待网页加载完全
+
+
+            var i = 0;
+            while (true)
+            {
+                try
+                {
+                    var element = chromeDriver.FindElementByXPath("//*[@id=\"comment_box\"]/div[3]/div/div[2]/a");
+                    element.Click();
+                    Thread.Sleep(3000);//等待网页加载完全
+                    Console.WriteLine("第{0}次点击",i.ToString());
+                    i++;
+                }
+                catch
+                {
+                    break;
+                }
+             }
+            IJavaScriptExecutor js = (IJavaScriptExecutor)chromeDriver;//页面滑倒最低端
+            string title = (string)js.ExecuteScript("window.scrollTo(0,document.body.scrollHeight)");
+            var page = chromeDriver.PageSource;
+            File.WriteAllText(Environment.CurrentDirectory + "\\周杰伦.txt", page);//网页写入txt文件
+            HtmlParse htmlParse = new HtmlParse();
+            htmlParse.GetContent(Environment.CurrentDirectory + "\\周杰伦.txt");
         }
-    }
+    } 
 }
