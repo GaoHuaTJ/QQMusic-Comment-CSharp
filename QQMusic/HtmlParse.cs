734029e83d7c92c6c282e919f050a7b710c067ec
qidaxiang66@icloud.com <qidaxiang66@icloud.com>
2019-02-24 16:30:11 +0800

    SongClass加入了歌手名的属性

--- QQMusic/HtmlParse.cs
+++ QQMusic/HtmlParse.cs
@@ -25,6 +25,14 @@ namespace QQMusic
             var dates = GetContents(path, "//span[@class='comment__date c_tx_thin']");
             var comments = GetContents(path, "//p[@class='c_tx_normal comment__text js_hot_text']");
             var agree = GetContents(path, "//span[@class='js_praise_num']");
+            if (IsExistTable(_SingerName))
+            {
+                Console.WriteLine("该数据表存在");
+            }
+            else
+            {
+                Console.WriteLine("数据库中没有表{0},已经新建表",_SingerName);
+            }
 
             for (int i = 0; i < users.Count; i++)
             {
@@ -33,17 +41,18 @@ namespace QQMusic
                     string sql = string.Format("INSERT INTO dbo.{0}(歌名,用户名,评论,评论时间,赞)VALUES('{1}','{2}','{3}','{4}','{5}')",
                         _SingerName,
                         _Song,
-                        users[i], 
-                        comments[i], 
-                        dates[i], 
+                        users[i],
+                        comments[i],
+                        dates[i],
                         agree[i]);
-                    ConnectAccess(sql);
+                    ConnectAccess(sql,out SqlConnection sqlConnection);
+                    Console.WriteLine("数据存储成功");
                 }
                 catch
-                {continue;}
+                { continue; }
             }
-            Console.ReadKey();
-            }
+
+        }
         public List<string> GetContents(string path,string Xpath)//文本内容存入集合
         {
                 List<string> Contents = new List<string> { };
@@ -65,14 +74,32 @@ namespace QQMusic
             HtmlNodeCollection htmlNodes = html.DocumentNode.SelectNodes(Xpath);
             return htmlNodes;
         }
-        public void ConnectAccess(string Sql)//数据库连接和sql执行
+        public SqlCommand ConnectAccess(string Sql,out  SqlConnection sqlConnection)//数据库连接和sql执行
         {
             string Conn = string.Format("Data Source=DESKTOP-49O35N0;Initial Catalog=C#练习使用;Integrated Security=True");
-            SqlConnection sqlConnection = new SqlConnection(Conn);
+            sqlConnection = new SqlConnection(Conn);
             sqlConnection.Open();
             SqlCommand sqlCommand = new SqlCommand(Sql, sqlConnection);
             sqlCommand.ExecuteReader();
-            Console.WriteLine("数据存储成功");
+            sqlConnection.Close();
+            return sqlCommand;//返回sql执行的数据库对象
+        }
+        public bool IsExistTable(string tableName)//判断C#练习数据库是否存在表名
+        {
+            var IsExistTableSql = string.Format("SELECT * FROM sys.tables WHERE name = '{0}'",tableName);
+           var sqlCommand= ConnectAccess(IsExistTableSql,out SqlConnection  sqlConnection);
+            sqlConnection.Open();
+            if (Convert.ToInt16(sqlCommand.ExecuteScalar()) == 0)//如果不存在表的话
+            {
+                var sql = string.Format("CREATE TABLE {0}(歌名 VARCHAR(max) NULL,用户名 VARCHAR(max) NULL,评论 VARCHAR(max) NULL,评论时间 VARCHAR(max) NULL,赞 INT NULL)", _SingerName);//创建一个新的数据表
+                ConnectAccess(sql,out SqlConnection sqlConnection1);
+                Console.WriteLine("{0}数据表创建成功",_SingerName);
+                return false;
+            }
+            else
+            {
+                return true;
+            }
         }
     }
 }
--- QQMusic/Program.cs
+++ QQMusic/Program.cs
@@ -15,9 +15,9 @@ namespace QQMusic
         {
             //实例化歌名类
             Song daoXiang = new Song();
-            daoXiang.SingerName = "周杰伦";
-            daoXiang.Name = "牛仔很忙";
-            daoXiang.NameUrl = "https://y.qq.com/n/yqq/song/003O5GaP1SYe94.html";
+            daoXiang.SingerName = "林俊杰";
+            daoXiang.Name = "她说";
+            daoXiang.NameUrl = "https://y.qq.com/n/yqq/song/0009n8YD3Nh2rR.html";
             var page=daoXiang.CreatDriver();//输出当前网址的源码
             var path=daoXiang.GetHtmlFold(page);//输出网页源码的txt地址
    
@@ -25,7 +25,5 @@ namespace QQMusic
             HtmlParse htmlParse = new HtmlParse(daoXiang.Name,daoXiang.SingerName);
             htmlParse.GetContent(path);
         }
-
-
     } 
 }
